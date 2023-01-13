db04b99371ed0aacb6b368be4ef697006c3401cf
Pavel Dzehtsiarenka <dragrais23@gmail.com>
2023-01-14 00:19:07 +0300

    init: reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (num) {
+    let arr = num.toString().split("");
+    let arr2 = "";
+    if (arr[0] === "-") {
+      for (let i = arr.length - 1; i >= 1; i--) {
+        arr2 = arr2 + arr[i];
+      }
+    } else {
+      for (let i = arr.length - 1; i >= 0; i--) {
+        arr2 = arr2 + arr[i];
+      }
+    }
+    return arr2;
 }
